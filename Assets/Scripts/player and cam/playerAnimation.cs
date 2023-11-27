28df536e351372011737323ec48ec9d31bccb6cb
Hanna Pak <hannacarlie@gmail.com>
2023-11-27 15:20:32 -0500

    basic player movement animations & sprite flipping done!

--- Assets/Scripts/player and cam/playerAnimation.cs
+++ Assets/Scripts/player and cam/playerAnimation.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class playerAnimation : MonoBehaviour
 {
+    // refs to other scripts
     public groundCheck GroundCheck;
     public Animator animator;
 
@@ -18,19 +19,19 @@ public class playerAnimation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (playerRB.velocity.x > 0)
+        if (!GroundCheck.grounded)
         {
-            Debug.Log("positive x");
-            animator.SetInteger("anim_state", 1);
-        }
-        else if (playerRB.velocity.x < 0)
-        {
-            Debug.Log("negative x");
+            // jump = 2
             animator.SetInteger("anim_state", 2);
+        }
+        else if (playerRB.velocity.x != 0)
+        {
+            // run = 1
+            animator.SetInteger("anim_state", 1);
         }
         else if (playerRB.velocity.x == 0)
         {
-            Debug.Log("at rest");
+            // idle = 0
             animator.SetInteger("anim_state", 0);
         }
     }
--- Assets/Scripts/player and cam/playerMovement.cs
+++ Assets/Scripts/player and cam/playerMovement.cs
@@ -5,31 +5,71 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
 
-    // inputs to speed & jump force
+    // inputs to speed & jump force, ref to groundCheck script
     public float moveSpeed;
-    [SerializeField] float jumpForce;
-
-    float horizontal;
+    public float jumpForce;
+    public groundCheck GroundCheck;
 
     Rigidbody2D playerRB;
 
-    public groundCheck GroundCheck;
+    private float horizontalInput;
+
+    // sprite flipping stuff
+    [HideInInspector]
+    public bool isFacingLeft;
+    public bool spawnFacingLeft;
+    // TODO: put spawnFacingLeft in separate player/level instantiation script (later)
+    private Vector2 facingLeft;
 
 
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
+        facingLeft = new Vector2(-gameObject.transform.localScale.x, gameObject.transform.localScale.y);
+        if(spawnFacingLeft)
+        {
+            gameObject.transform.localScale = facingLeft;
+            isFacingLeft = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerRB.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, playerRB.velocity.y);
+        horizontalInput = Input.GetAxis("Horizontal");
+        // horizontal movement
+        playerRB.velocity = new Vector2(horizontalInput * moveSpeed, playerRB.velocity.y);
 
+        // flip sprite if needed
+        if (horizontalInput > 0 && isFacingLeft)
+        {
+            isFacingLeft = false;
+            flipPlayer();
+        }
+        else if (horizontalInput < 0 && !isFacingLeft)
+        {
+            isFacingLeft = true;
+            flipPlayer();
+        }
+
+        // jump mechanics
         if ((Input.GetKeyDown("up") || Input.GetKeyDown("w")) && GroundCheck.grounded)
         {
             playerRB.AddForce(new Vector2(playerRB.velocity.x, jumpForce * 10));
+        }
+    }
+
+    // flips player according to isFacingLeft
+    void flipPlayer()
+    {
+        if (isFacingLeft)
+        {
+            gameObject.transform.localScale = facingLeft;
+        }
+        else if (!isFacingLeft)
+        {
+            gameObject.transform.localScale = new Vector2(-gameObject.transform.localScale.x, gameObject.transform.localScale.y);
         }
     }
 }
