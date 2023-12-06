9bd5898e615559f6456181da63581dcaa8c6077f
Emmet Halm <emmet.halm@gmail.com>
2023-12-05 20:08:18 -0500

    update bank dialog + menu buttons

--- Assets/Scripts/bank/bankMenu.cs
+++ Assets/Scripts/bank/bankMenu.cs
@@ -1,47 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class bankMenu : MonoBehaviour
 {
-    [SerializeField] private GameObject Deposit;
-    [SerializeField] private GameObject Exit;
-    [SerializeField] private GameObject Info;
+    [SerializeField] private Button Deposit;
+    [SerializeField] private Button Exit;
+    [SerializeField] private Button Info;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Deposit.onClick.AddListener(DepositOnClick);
+        Exit.onClick.AddListener(ExitOnClick);
+        Info.onClick.AddListener(InfoOnClick);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Deposit != null)
-        {
-            TextMesh depositTextMesh = Deposit.GetComponent<TextMesh>();
-            if (depositTextMesh != null)
-            {
-                depositTextMesh.text = "Deposit";
-            }
-        }
+    }
 
-        if (Exit != null)
-        {
-            TextMesh exitTextMesh = Exit.GetComponent<TextMesh>();
-            if (exitTextMesh != null)
-            {
-                exitTextMesh.text = "Exit";
-            }
-        }
+    void DepositOnClick()
+    {
+        // Display deposit popup
+        Debug.Log("Deposit clicked");
+    }
 
-        if (Info != null)
-        {
-            TextMesh infoTextMesh = Info.GetComponent<TextMesh>();
-            if (infoTextMesh != null)
-            {
-                infoTextMesh.text = "Info";
-            }
-        }
+    void ExitOnClick()
+    {
+        // Return to previous level
+        Debug.Log("Exit clicked");
+    }
+
+    void InfoOnClick()
+    {
+        // Display info popup
+        Debug.Log("Info clicked");
     }
 }
+
